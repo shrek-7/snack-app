810fba6fc16354554d09419681cde6eaf62bc00e
shreyas <shreyas@moonraft.com>
2018-01-17 13:15:51 +0530

    mobile view

--- client/src/components/dashboard/components/main-page/index.js
+++ client/src/components/dashboard/components/main-page/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import $ from 'jquery';
+
 import ItemTile from './components/item-tile';
 import DrinksTile from './components/drinks-tile';
 import Header from '../../../header';
@@ -22,10 +24,12 @@ export default class MainPage extends Component {
       data:[
         {name:"Dominoes"},{name:"Pizza Hut"},{name:"MacDonalds"}
       ],
-      drinks: [{src:bud,name:"budweiser magnum(strong)"},{src:biraW,name:"bira white(light)"},{src:carls,name:"Carlsberg elephant(strong)"}]
+      drinks: [{src:bud,name:"budweiser magnum"},{src:biraW,name:"bira white"},{src:carls,name:"Carlsberg elephant"}]
     }
     this.renderTile=this.renderTile.bind(this);
     this.renderDrinks=this.renderDrinks.bind(this);
+    this.scrollToDrinks=this.scrollToDrinks.bind(this);
+    this.scrollToSnacks=this.scrollToSnacks.bind(this);
   }
   componentDidMount(){
     //make api call
@@ -56,19 +60,39 @@ export default class MainPage extends Component {
       })
     );
   }
+
+  scrollToDrinks(){
+    $('.nav__snacks').removeClass('nav__active');
+    $('.nav__drinks').addClass('nav__active');
+    $('html, body').animate({
+      scrollTop: $('#drinks').offset().top
+  }, 1000);
+  }
+  scrollToSnacks(){
+    $('.nav__drinks').removeClass('nav__active');
+    $('.nav__snacks').addClass('nav__active');
+    $('html, body').animate({
+      scrollTop: $("#snacks").offset().top-150
+  }, 1000);
+  }
+
   render() {
     return (
         <div id="main" className="main__wrapper">
         <Header/>
+        <div className='nav navigation__wrap'>
+          <div onClick={()=>this.scrollToSnacks()} className='nav__snacks nav__active'>Snacks</div>
+          <div onClick={()=>this.scrollToDrinks()} className='nav__drinks'>Drinks</div>
+        </div>
         <div className="snacks-wrapper">
-          <div className="tile-wrapper snacks-wrapper__food">
+          <div className="tile-wrapper snacks-wrapper__food" id="snacks">
             {this.renderTile()}
             {/* <ItemTile src={dessert} />
             <ItemTile src={kebab} />
             <ItemTile src={steak} /> */}
           </div>
           <div className="separator"></div>
-          <div className="snacks-wrapper__drinks">
+          <div className="snacks-wrapper__drinks" id="drinks">
             {this.renderDrinks()}
           </div>
         </div>
